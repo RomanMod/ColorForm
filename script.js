2135ff20c0875c23ebad5424da59c1a453778143
MoodRa <58891836+RomanMod@users.noreply.github.com>
2025-05-09 19:45:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -696,7 +696,7 @@ function handleVisionChoice(event) {
     if (visionMode === 'color' && visionResultDisplay) {
         visionResultDisplay.style.backgroundColor = visionCurrentResult;
         let messageText = document.createElement('p');
-        messageText.textContent = isCorrect ? `Успех!` : `Далее!`;
+        messageText.textContent = isCorrect ? `Успех!` : `Отвлекся!`;
         messageText.style.color = 'white';
         messageText.style.textShadow = '1px 1px 3px rgba(0,0,0,0.5)';
         visionResultDisplay.appendChild(messageText);
@@ -709,7 +709,7 @@ function handleVisionChoice(event) {
         const svg = visionCurrentResult === 'circle' ? cachedElements.svgCircle : cachedElements.svgTriangle;
         feedbackContent.appendChild(svg.cloneNode(true));
         const messageText = document.createElement('p');
-        messageText.textContent = isCorrect ? `Успех!` : `Далее`;
+        messageText.textContent = isCorrect ? `Успех!` : `Отвлекся`;
         messageText.style.color = 'black';
         feedbackContent.appendChild(messageText);
         visionResultDisplay.appendChild(feedbackContent);
